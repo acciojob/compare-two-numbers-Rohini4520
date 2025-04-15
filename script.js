9ce968fc27d54b971e1574560ce1eba6bfa60c85
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-04-15 21:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let num1 = prompt("Enter the First number");
+let num2 = prompt("Enter the second number");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(num1 == num2);
